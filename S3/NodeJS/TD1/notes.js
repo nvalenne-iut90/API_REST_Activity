90a4643c1ce500df0925881a125402b54f301596
nvalenne-iut90 <nathan.valenne@edu.univ-fcomte.fr>
2022-09-12 09:25:32 +0200

    TP2 Express NodeJS

--- S3/NodeJS/TD1/notes.js
+++ S3/NodeJS/TD1/notes.js
@@ -1,41 +1,37 @@
-const fs = require('fs');
-const chalk = require('chalk');
+const fs = require("fs");
+const chalk = require("chalk");
 
 const ajouterNote = (title, body) => {
-    // vérifier si la note existe
-    // lire toutes les notes
+    //verifier si la note existe
+    //lire toutes les notes
     const notes = lireNotes();
-    const duplicate = notes.find(
-        (note) => note.title === title
-    );
+    const duplicate = notes.find((note) => note.title === title)
     if (!duplicate){
-        notes.push({
-            title : title,
-            body : body
-        });
-        // sauvegarder avec le fichier notes.json
+        notes.push({title:title, body:body});
+        //sauvegarder dans le fichier notes.json
         saveNote(notes);
-        console.log(chark.green(
-            "Sauvegarde effectué !"
-        ))
-    } else {
-        console.log(chark.red(
-            "Erreur de sauvegarde"
-        ))
+        console.log(chalk.green("Opération réussi !!"))
+    }else {
+        console.log(chalk.red("Duplicate !"))
     }
+    
+}
 
+const lireNotes = () => {
+    try{
+        const data = fs.readFileSync("notes.json");
+        const dataJSON = data.toString();
+        return JSON.parse(dataJSON);
+    }catch(e){
+        return [];
+    }
 }
 
 const saveNote = (notes) => {
     const data = JSON.stringify(notes);
-    fs.writeFileSync("notes.json", data)
+    fs.writeFileSync("notes.json", data);
 }
 
-const lireNotes = () => {
-    try {
-        const dataJSON = fs.readFileSync("notes.json").toString();
-        return JSON.parse(dataJSON);
-    } catch(e) {
-        return [];
-    }
+module.exports = {
+    ajouterNote: ajouterNote
 }
